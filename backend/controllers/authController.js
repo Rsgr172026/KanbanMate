d939db0c00834ac7a6916e920528098d00829453
Rsgr172026 <rsagar1720@gmail.com>
2026-01-22 17:55:53 +0530

    Force Secure True

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,8 +6,8 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
@@ -74,8 +74,8 @@ exports.googleLogin = async (req, res) => {
 
     res.cookie("jwt", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "None",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
